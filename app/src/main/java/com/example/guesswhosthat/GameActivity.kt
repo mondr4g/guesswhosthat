ae839debc4133993c82f0e16d0451047b51c698a
RoderickNight <rodermaddness@gmail.com>
2021-12-02 14:47:19 -0600

    cambios de clock y musik

--- app/src/main/java/com/example/guesswhosthat/GameActivity.kt
+++ app/src/main/java/com/example/guesswhosthat/GameActivity.kt
@@ -1,5 +1,6 @@
 package com.example.guesswhosthat
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
@@ -17,7 +18,6 @@ import com.example.guesswhosthat.Session.LoginPref
 import okhttp3.ResponseBody
 import java.lang.Exception
 import android.app.Activity
-import android.content.Intent
 import android.widget.Button
 
 
@@ -56,6 +56,10 @@ class GameActivity : AppCompatActivity() {
         session = LoginPref(this)
         user  = session.getUserDetails()
         val btn_pressed = intent.getStringExtra(BTN_PRESSED)
+
+        val mIntent = Intent()
+        mIntent.action="Battle"
+        sendBroadcast(mIntent)
 
         when (btn_pressed) {
             "0" -> prepare1vs1()
@@ -142,7 +146,10 @@ class GameActivity : AppCompatActivity() {
     fun prepare1vsFriends() {
         setContentView(R.layout.activity_board1vs1)
         chrono = findViewById(R.id.chronos)
-
+        chrono.setOnChronometerTickListener{
+            changeData()
+        }
+        chrono.start()
     }
 
     fun prepare1vsAI() {
@@ -209,20 +216,17 @@ class GameActivity : AppCompatActivity() {
         recView4.adapter = adapter4
 
         chrono.setOnChronometerTickListener{
-            val time = SystemClock.elapsedRealtime() - chrono.base
-            val h = (time / 3600000).toInt()
-            val m = (time - h * 3600000).toInt() / 60000
-            val s = (time - h * 3600000 - m * 60000).toInt() / 1000
-            val hh = if (h < 10) "0$h" else h.toString() + ""
-            val mm = if (m < 10) "0$m" else m.toString() + ""
-            val ss = if (s < 10) "0$s" else s.toString() + ""
-            chrono.text = "$hh:$mm:$ss"
+            changeData()
         }
+        chrono.start()
     }
-/*
-    @Override
+
+    /*@Override
     override fun onStart(){
         super.onStart()
+        chrono.setOnChronometerTickListener{
+            changeData()
+        }
         chrono.start()
     }*/
 
@@ -257,5 +261,6 @@ class GameActivity : AppCompatActivity() {
     override fun onBackPressed() {
         super.onBackPressed()
         Characters.emptyCharsList()
+        chrono.stop()
     }
 }
